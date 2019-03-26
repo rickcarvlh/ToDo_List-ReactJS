58426dd3ab435c79a22d36e14536fb46cbc8bc54
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2019-03-26 10:25:19 +0000

    creating map function

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -5,10 +5,33 @@ import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+          {
+            id:1,
+            title: 'Take out the trash',
+            completed: false
+          },
+          {
+            id:2,
+            title: 'Dinner with wife',
+            completed: false
+          },
+          {
+            id:3,
+            title: 'Metting with boss',
+            completed: false
+          }
+    ]
+  }
+
+
   render() {
+    console.log(this.state.todos);
+    
     return (
       <div className="App">
-        <Todos/>
+        <Todos todos = {this.state.todos}/>
       </div>
     );
   }
