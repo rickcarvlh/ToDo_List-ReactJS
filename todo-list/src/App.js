879796377c6b641aac9961c08e0e69c00ef446ed
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2019-03-27 18:46:31 +0000

    random id

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Todos from "./components/Todos";
 import Header from "./components/layout/Header";
 import AddTodo from "./components/AddTodo";
+import uuid from 'uuid';
 
 import "./App.css";
 
@@ -9,17 +10,17 @@ class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: "Take out the trash",
         completed: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: "Dinner with wife",
         completed: true
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: "Metting with boss",
         completed: false
       }
@@ -66,17 +67,16 @@ class App extends Component {
   };
 
   // Add Todo
-  addTodo = (title) =>{
+  addTodo = title => {
     const newTodo = {
-      id: 4,
+      id: uuid.v4(),
       title: title,
       completed: false
-    }
+    };
     this.setState({
       todos: [...this.state.todos, newTodo]
-    })
-    
-  }
+    });
+  };
 
   render() {
     // console.log(this.state.todos);
