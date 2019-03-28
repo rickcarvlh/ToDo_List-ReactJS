3c0d09617319c0484f278f6f98f7dedc0eee5583
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2019-03-28 18:09:29 +0000

    styling routing classes

--- todo-list/src/components/layout/Header.js
+++ todo-list/src/components/layout/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header style={headerStyle}>
         <h1>TodoList</h1>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+        <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link>
     </header>
   )
 }
@@ -15,4 +15,9 @@ const headerStyle = {
     color: '#fff',
     textAlign: 'center',
     paddding: '10px'
+}
+
+const linkStyle = {
+  color: '#fff',
+  textDecoration: 'none'
 }
