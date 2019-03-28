69bd5a5814749e6a85f7f2baf7c0d3eda939326a
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2019-03-28 18:45:14 +0000

    add and delete to the server

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -4,16 +4,22 @@ import Todos from "./components/Todos";
 import Header from "./components/layout/Header";
 import AddTodo from "./components/AddTodo";
 import About from "./components/pages/About";
-import uuid from 'uuid';
+// import uuid from 'uuid';
+import axios from "axios";
 
 import "./App.css";
 
+
 class App extends Component {
   state = {
-    todos: [
-    
-    ]
+    todos: []
   };
+
+  // isto é uma promise
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then(res => this.setState({todos: res.data}));
+  }
 
   // * chegou-se finalmente a este lvl desde o TodoItems porque o estado está aqui e não
   // * em TodoItems (state)
@@ -49,21 +55,19 @@ class App extends Component {
    *  porque esse é o que nos queremos apagar
    */
   delTodo = id => {
-    this.setState({
-      todos: [...this.state.todos.filter(todo => todo.id !== id)]
-    });
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res => this.setState({ todos: [...this.state.todos.filter
+        (todo => todo.id !== id)]}));
   };
 
   // Add Todo
-  addTodo = title => {
-    const newTodo = {
-      id: uuid.v4(),
-      title: title,
+  addTodo = (title) => {
+    axios.post("https://jsonplaceholder.typicode.com/todos", {
+      title,
       completed: false
-    };
-    this.setState({
-      todos: [...this.state.todos, newTodo]
-    });
+    })
+      .then(res => this.setState({
+        todos: [...this.state.todos, res.data]}));
   };
 
   render() {
